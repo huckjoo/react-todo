99eea3f30fa7ebf5f87acda971f3592d9322a13f
huckjoo <omp0109@naver.com>
2023-07-21 15:30:09 +0900

    feat: 클릭, 엔터로 Todo 추가하도록 수정

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,17 +2,29 @@ import React, { useState } from 'react';
 import styles from './Footer.module.css'
 
 export default function Footer({ handleClick }) {
-  const [value, setValue] = useState();
+  const [value, setValue] = useState("");
 
   const handleChange = (e) => {
     setValue(e.target.value);
   }
 
+  const handleButtonClick = () => {
+    handleClick(value)
+    setValue("")
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!value) return
+    handleClick(value)
+    setValue("")
+  }
+
   return (
     <div className={styles.footer}>
-      <form className={styles.form}>
-        <input className={styles.input} placeholder='Add ToDo' onChange={handleChange} />
-        <button type="button" className={styles.button} onClick={() => { handleClick(value) }}>Add</button>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input className={styles.input} placeholder='Add ToDo' onChange={handleChange} value={value} />
+        <button type="button" className={styles.button} onClick={handleButtonClick}>Add</button>
       </form>
     </div>
   );
