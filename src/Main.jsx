216cad121ed03383688fe2932c4dc5ee056fa796
huckjoo <omp0109@naver.com>
2023-07-29 20:07:36 +0900

    feat: 스크롤 추가, main 디자인 수정

--- src/Main.jsx
+++ src/Main.jsx
@@ -5,31 +5,34 @@ export default function Main({ todos, handleDelete, types, handleUpdate }) {
 
   return (
     <div className={styles.main}>
-      <ul>
-        {types === 'active' && todos.filter(todo => todo.checked === false).map((todo, i) => {
+      {types === 'active' && <ul>
+        {todos.filter(todo => todo.checked === false).map((todo, i) => {
           return (
             <li key={i}>
               <span>{todo.value}</span>
             </li>
           )
         })}
-      </ul>
+      </ul>}
 
-      <ul>
-        {types === 'completed' && todos.filter(todo => todo.checked === true).map((todo, i) => {
+      {types === 'completed' && <ul>
+        {todos.filter(todo => todo.checked === true).map((todo, i) => {
           return (
             <li key={i}>
               <span>{todo.value}</span>
             </li>
           )
         })}
-      </ul>
+      </ul>}
+
 
       {types === 'all' && todos.map((todo, i) => {
         return (
-          <div key={i}>
-            <input type="checkbox" onChange={(e) => { handleUpdate(i, e.target.checked) }} checked={todo.checked} />
-            <span>{todo.value}</span>
+          <div className={styles.list} key={i}>
+            <div>
+              <input type="checkbox" onChange={(e) => { handleUpdate(i, e.target.checked) }} checked={todo.checked} />
+              <span>{todo.value}</span>
+            </div>
             <button className={styles.button} onClick={() => { handleDelete(i) }}>X</button>
           </div>
         )
