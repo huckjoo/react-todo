4682fb84803bb7a51a0a1e1bd4722563425f8e53
huckjoo <omp0109@naver.com>
2023-10-03 00:56:03 +0900

    style: todo 꾸미기

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaTrashAlt } from 'react-icons/fa';
+import styles from './Todo.module.css'
 
 export default function Todo({ todo, onUpdate, onDelete }) {
   const { text, status } = todo;
@@ -9,12 +10,14 @@ export default function Todo({ todo, onUpdate, onDelete }) {
   }
   const handleDelete = () => onDelete(todo);
 
-  return <li>
-    <input type='checkbox' id='checkbox' checked={status === 'completed'} onChange={handleChange} />
-    <label htmlFor='checkbox'>{text}</label>
-    <button onClick={handleDelete}>
-      <FaTrashAlt />
-    </button>
-  </li>
+  return <li className={styles.todo}>
+    <input className={styles.checkbox} type='checkbox' id='checkbox' checked={status === 'completed'} onChange={handleChange} />
+    <label className={styles.text} htmlFor='checkbox'>{text}</label>
+    <span className={styles.icon}>
+      <button className={styles.button} onClick={handleDelete}>
+        <FaTrashAlt />
+      </button>
+    </span>
+  </li >
 }
 
