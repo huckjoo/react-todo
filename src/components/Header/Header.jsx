346389f1efaf31519a60ab77f46d137fa1065662
huckjoo <omp0109@naver.com>
2023-10-07 23:23:16 +0900

    style: 다크모드 아이콘 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import styles from './Header.module.css'
+import { FaSun } from 'react-icons/fa'
 
 export default function Header({ filters, filter, onFilterChange }) {
   return <header className={styles.header}>
+    <span className={styles.icon}>
+      <button className={styles.button}>
+        <FaSun />
+      </button>
+    </span>
     <ul className={styles.filters}>
       {filters.map((value, index) => (
         <li key={index} >
